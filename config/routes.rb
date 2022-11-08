6c42c2adf969c2cbcb839099e9f29487b65bc74d
Tash Cruz <sebastiantashika@gmail.com>
2022-11-08 12:33:21 +0900

    added new, and index html in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # route for all the task
+  get '/tasks', to: 'tasks#index', as: :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # route to create new tasks
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
 end
