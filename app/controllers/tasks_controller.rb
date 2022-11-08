754de3d882ab1e482cf20241d4b97d7260b4e1ed
Tash Cruz <sebastiantashika@gmail.com>
2022-11-08 15:07:57 +0900

    added edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -16,9 +21,23 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   # route to create new tasks
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
+
+  # routes to show task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # route to edit task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # route
 end
